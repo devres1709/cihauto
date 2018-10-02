74289566a1d70afbd34a149879c2f7d84879422a
Evgeny Ryabyshew <ru.evgeny.dev@gmail.com>
2018-10-02 15:02:22 +0300

    fire alarm

---
+++ src/test/java/CihBuilderTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class CihBuilderTest {
+    @Test
+    public void doBuild() throws IOException, InterruptedException {
+        CihBuilder.build();
+    }
+
+}
